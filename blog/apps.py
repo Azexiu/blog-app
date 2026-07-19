d2a9890170ef80af4baff5a50d9f74de8a9749ba
Azexiu <connorskullheart@gmail.com>
2026-07-19 15:27:19 -0300

    Post Modelos

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,7 @@
-from django.apps import AppConfig
+from django.contrib import admin
+from .models import Post, Tag, Author
 
 
-class BlogConfig(AppConfig):
-    name = 'blog'
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Author)
