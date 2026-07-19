2cd11288429442bf3fc7aea3e2ffbeef5dc552a8
Azexiu <connorskullheart@gmail.com>
2026-07-19 15:31:43 -0300

    Solucion Error

--- blog/apps.py
+++ blog/apps.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from .models import Post, Tag, Author
+from django.apps import AppConfig
 
 
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Author)
+class CoreConfig(AppConfig):
+    name = 'core'
